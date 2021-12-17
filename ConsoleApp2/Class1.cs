92ad985b9fff877164ac9bd627bc09c778723e46
Тарас Русакович <tarus01@mail.ru>
2021-12-17 15:09:46 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Class1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace queue
+{
+    public class Deque<T>
+    {
+        private LinkedList<T> Link_List = new LinkedList<T>();
+
+        public delegate void CollHandler(string message);                   //event declaration
+        public event CollHandler Notify;
+
+        public void EnqueueFirst(T item)
+        {
+            Link_List.AddFirst(item);
+            Notify?.Invoke($"Item {item} has been added as the first one");
+        }
+
+        public void EnqueueLast(T item)
+        {
+            Link_List.AddLast(item);
+            Notify?.Invoke($"Item {item} has been added as the last one");
+        }
+
+        public T DequeueFirst()
+        {
+            if(Link_List.Count == 0)
+            {
+                throw new InvalidOperationException("DequeueFirst called when deque is empty");
+            }
+
+            T temp = Link_List.First.Value;
+            Notify?.Invoke($"The first item {Link_List.First.Value} has been removed");
+            Link_List.RemoveFirst();
+            return temp;
+        }
+
+        public T DequeueLast()
+        {
+            if (Link_List.Count == 0)
+            {
+                throw new InvalidOperationException("DequeueLast called when deque is empty");
+            }
+
+            T temp = Link_List.Last.Value;
+            Notify?.Invoke($"The last item {Link_List.Last.Value} has been removed");
+            Link_List.RemoveLast();
+            return temp;
+        }
+
+        public T PeekFirst()
+        {
+            if (Link_List.Count == 0)
+            {
+                throw new InvalidOperationException("PeekFirst called when deque is empty");
+            }
+
+            Notify?.Invoke($"The first item {Link_List.First.Value} has been peeked");
+            return Link_List.First.Value;
+        }
+
+        public T PeekLast()
+        {
+            if (Link_List.Count == 0)
+            {
+                throw new InvalidOperationException("PeekLast called when deque is empty");
+            }
+
+            Notify?.Invoke($"The last item {Link_List.Last.Value} has been peeked");
+            return Link_List.Last.Value;
+        }
+
+        public int Count()
+        {
+            return Link_List.Count;
+        }
+
+        public void Clear()
+        { 
+            Link_List.Clear();
+            Notify?.Invoke($"The collection has been deleted");
+        }
+
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deque<string> Deq = new Deque<string>();
+            Deq.Notify += DisplayMessage;
+
+            Console.WriteLine("Add elements to Stack and press Enter to finish" +
+                              "\npress 1 to add the first one" +
+                              "\npress 2 to add the last one" +
+                              "\npress 3 to remove the first one" +
+                              "\npress 4 to remove the last one");
+
+            bool temp = true;
+            while (temp == true)
+            {
+                try
+                {
+                    switch (Console.ReadKey(true).Key)
+                    {
+
+                        case ConsoleKey.D1:
+                            {
+                                Console.Write("member:");
+                                Deq.EnqueueFirst(Console.ReadLine());
+                                break;
+                            }
+
+                        case ConsoleKey.D2:
+                            {
+                                Console.Write("member:");
+                                Deq.EnqueueLast(Console.ReadLine());
+                                break;
+                            }
+
+                        case ConsoleKey.D3:
+                            {
+                                string t = Deq.DequeueFirst();
+                                break;
+                            }
+
+                        case ConsoleKey.D4:
+                            {
+                                string t = Deq.DequeueLast();
+                                break;
+                            }
+
+                        case ConsoleKey.Enter:
+                            {
+                                temp = false;
+                                break;
+                            }
+
+                        default:
+                            {
+                                Console.WriteLine("Please, choose what you want to do");
+                                break;
+                            }
+                    }
+                }
+                catch (InvalidOperationException ex) 
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"{ex.Message}"); 
+                    Console.ResetColor();
+                    continue;
+                }
+
+                catch
+                {
+                    Console.WriteLine("try again");
+                    continue;
+                }
+
+            }
+
+            Console.WriteLine($"Size: {Deq.Count()}");
+            Deq.Clear();
+
+
+            Console.ReadKey();
+
+
+
+        }
+
+            private static void DisplayMessage(string message)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(message);
+                    Console.ResetColor();
+                }
+
+        
+
+    }
+}
